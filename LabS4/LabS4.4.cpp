92b148d71a25a3e603b268f2a27f6defdd227573
yotsubasuzu <74945325+yotsubasuzu@users.noreply.github.com>
2020-12-02 12:50:34 +0700

    Create LabS4.4.cpp

---
+++ LabS4/LabS4.4.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	int a,b,c;
+	do{
+	
+	printf("Nhap 1 so:");
+	scanf("%d",&a);
+	printf("Nhap 1 so:");
+	scanf("%d",&b);
+	printf("Nhap 1 so:");
+	scanf("%d",&c);
+	}
+	while(!(a+b>c && a+c>b && b+c>a && a>0 && b>0 && c>0));
+		printf("\nDay la 3 canh tam giac");
+	int p=a+b+c;
+	float s=sqrt(p*(p-a)*(p-b)*(p-c));
+	printf("\nChu vi: %d va Dien tich: %f",p,s);
+}
