35f28007b0573fe951fe4e2c250818a23f903b32
yotsubasuzu <74945325+yotsubasuzu@users.noreply.github.com>
2020-12-02 12:50:28 +0700

    Create LabS4.1.cpp

---
+++ LabS4/LabS4.1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+	int n,x=1;
+	printf("Nhap 1 so n=");
+	scanf("%d",&n);
+
+	for(int i=1;i<=n;i++){
+		x = x * i;
+	}
+	
+//	int i=1;
+//	do{
+//		x = x*i;
+//		i++;
+//	}while(i<=n);
+
+	printf("n! = %d",x);
+}
