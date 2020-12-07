9da333871f45170404d05f6822e5197e7511bd87
yotsubasuzu <74945325+yotsubasuzu@users.noreply.github.com>
2020-12-07 13:29:04 +0700

    Create Assignment3.3.cpp

---
+++ Assignment3/Assignment3.3.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+	int a,b;
+	printf("Nhap so a: ");
+	scanf("%d",&a);
+	printf("Nhap so b: ");
+	scanf("%d",&b);
+	
+	if(a<b){
+		for(int i=a;i>=1;i--){
+			if(a%i==0 && b%i==0){
+				printf("Uoc chung lon nhat cua %d va %d la %d",a,b,i);
+				break;
+			}
+		}
+	}else{
+		for(int i=b;i>=1;i--){
+			if(a%i==0 && b%i==0){
+				printf("Uoc chung lon nhat cua %d va %d la %d",a,b,i);
+				break;
+			}
+		}
+	}
+}
