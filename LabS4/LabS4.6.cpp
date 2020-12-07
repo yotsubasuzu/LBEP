45367d3d101db4f3cfda33d86eb311ae7fd2b3de
yotsubasuzu <74945325+yotsubasuzu@users.noreply.github.com>
2020-12-07 13:28:29 +0700

    Update LabS4.6.cpp

--- LabS4/LabS4.6.cpp
+++ LabS4/LabS4.6.cpp
@@ -1,11 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
+//	int n;
+//	printf("Nhap 1 so n= ");
+//	scanf("%d",&n);
+//	
+//	int f;
+//	f=1/sqrt(5)*(pow(((1+sqrt(5))/2),n)-pow(((1-sqrt(5))/2),n));
+//	printf("So f(%d) trong day Fibonacci la %d",n,f);
+
 	int n;
-	printf("Nhap 1 so n= ");
+	printf("Nhap n:");
 	scanf("%d",&n);
 	
-	int f;
-	f=1/sqrt(5)*(pow(((1+sqrt(5))/2),n)-pow(((1-sqrt(5))/2),n));
-	printf("So f(%d) trong day Fibonacci la %d",n,f);
+	if(n<=0){
+		printf("So thu %d la: %d",n,0);	
+	}else if(n<=2){
+		printf("So thu %d la: %d",n,1);
+	}else{
+		int x=0,y=1,z=1;
+		for(int i=3;i<=n;i++){
+			x=y;
+			y=z;
+			z=x+y;
+		}
+		printf("So thu %d la: %d",n,z);
+	}
 }
