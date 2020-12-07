107b16a025528614aa94e4a08fdca5f56e1900b6
yotsubasuzu <74945325+yotsubasuzu@users.noreply.github.com>
2020-12-07 13:28:38 +0700

    Update LabS4.3.cpp

--- LabS4/LabS4.3.cpp
+++ LabS4/LabS4.3.cpp
@@ -1,8 +1,14 @@
 #include <stdio.h>
 int main(){
-	int n;
-	for(int i=1;i<=200;i+=2){
-		n = n + i;
+	int s = 0;
+//	for(int i=1;i<200;i+=2){
+//		s += i;
+//	}
+	for(int i=0,j=0;i<100;j++){
+		if(j%2!=0){
+			s+=j;
+			i++;
+		}
 	}
-	printf("Tong 100 so le dau tien >0: %d",n);
+	printf("Tong 100 so le dau tien >0: %d",s);
 }
