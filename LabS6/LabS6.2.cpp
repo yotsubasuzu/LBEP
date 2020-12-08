d84c2b62fc32caf2877b6ec7048678162e93ec0f
yotsubasuzu <74945325+yotsubasuzu@users.noreply.github.com>
2020-12-08 12:37:16 +0700

    Create LabS6.2.cpp

---
+++ LabS6/LabS6.2.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main(){
+	int n;
+	printf("Nhap so: ");
+	scanf("%d",&n);
+	
+	int ary[n];
+	
+	for(int i=0;i<n;i++){
+		printf("Nhap phan tu ary[%d]: ",i);
+		scanf("%d",&ary[i]);
+
+//		if(ary[i]%2!=0){
+//			k++;
+//			s=(s+ary[i]);
+//		}
+	}
+	int s=0,k=0;
+	for(int i=0;i<n;i++){
+		if(ary[i]%2!=0 && i%2==0){
+		k++;
+		s+=s+ary[i];
+		}
+	}
+	s=s/(float)k;
+	if(k!=0){
+		printf("Trung binh cong cua cac so le la: %f",s);
+	}else{
+		printf("Mang ko co so le o vi tri chan")
+	}
+//	printf("\nso K= %d",k);
+}
