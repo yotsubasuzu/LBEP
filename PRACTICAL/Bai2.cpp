ab3062d42c43d2079d3681d2637d67afba1f0b71
yotsubasuzu <74945325+yotsubasuzu@users.noreply.github.com>
2020-12-21 19:30:37 +0700

    Create Bai2.cpp

---
+++ PRACTICAL/Bai2.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+void upperCase(char s[],int n){
+	for(int i=0;i<strlen(s);i++){
+		if(s[i]>=97 && s[i]<=122)
+			s[i]-=32;
+	}
+}
+	
+
+int main(){
+	char s[100];
+	printf("Nhap chuoi: ");
+	gets(s);
+	
+	upperCase(s,100);
+	printf("Sau khi bien doi: %s",s);
+}
