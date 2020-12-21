9ac76b0cdd3236207bd3cca331cd539b7caf814e
yotsubasuzu <74945325+yotsubasuzu@users.noreply.github.com>
2020-12-21 19:37:06 +0700

    Update Bai2.cpp

--- PRACTICAL/Bai2.cpp
+++ PRACTICAL/Bai2.cpp
@@ -7,13 +7,10 @@ void upperCase(char s[],int n){
 			s[i]-=32;
 	}
 }
-	
-
 int main(){
 	char s[100];
 	printf("Nhap chuoi: ");
 	gets(s);
-	
 	upperCase(s,100);
 	printf("Sau khi bien doi: %s",s);
 }
